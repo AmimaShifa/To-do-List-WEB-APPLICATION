64aaa8e3a9c6c399ca08f5b4dcf64b576d8004aa
Amima Shifa <31186471+AmimaShifa@users.noreply.github.com>
2021-05-21 00:35:25 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDn6RXl-z_syvZdhyD7LSNeY20xmjjYO7U",
-    authDomain: "kanban-fire-92e87.firebaseapp.com",
-    projectId: "kanban-fire-92e87",
-    storageBucket: "kanban-fire-92e87.appspot.com",
-    messagingSenderId: "570858225653",
-    appId: "1:570858225653:web:910a0a3d235801ea8deabc",
-    measurementId: "G-JPQY55QBMH"
+   apiKey: '<your-key>',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: '<your-database-URL>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>'
   }
 };
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
